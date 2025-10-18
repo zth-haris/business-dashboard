de5e52f2c8a488ae0df90fee924f75c4ded4fe73
zth-haris <haris@zthtraining.com>
2025-10-18 01:32:16 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import { BarChart, Bar, LineChart, Line, PieChart, Pie, Cell, XAxis, YAxis, Cart
 import { TrendingUp, TrendingDown, DollarSign, Users, MousePointer, Mail, RefreshCw, Target, Eye, ShoppingCart } from 'lucide-react';
 
 // DEMO DATA - Replace this URL with your Make.com JSON file URL
-const DATA_URL = 'REPLACE_WITH_YOUR_JSON_URL';
-
+const DATA_URL = 'https://gist.githubusercontent.com/zth-haris/37a708a7d2805067c96ed8baa6cd609f/raw/2c4480682cab08f435d280cfb8a5755f5fd53833/whop-data.json';
 const demoData = {
   ltv: 487.50,
   totalRevenue: 45780,
